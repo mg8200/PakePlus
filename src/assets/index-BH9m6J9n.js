54488a8e86702eeb60b28d713c1a3305a81f4b60
mg8200 <73623971+mg8200@users.noreply.github.com>
2025-06-17 11:12:06 +0800

    update src file from pakeplus

---
+++ src/assets/index-BH9m6J9n.js
@@ -0,0 +1,1 @@
+import{d as e,o as a,e as t}from"./index-B3PZsRwT.js";const n=e({name:"quality-management_weekly-report-scrap-report",__name:"index",setup:e=>(e,n)=>(a(),t("div",null," 11 "))});export{n as default};
